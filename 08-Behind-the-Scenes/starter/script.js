fd2711527261ec7840659186521b715ab90d2ea6
Fanni Farkas <farkasfannikatalin@gmail.com>
2023-07-30 11:38:56 +0200

    practice scoping

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2023 - birthYear;
+  function printAge() {
+    const output = `${firstName} You are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const str = `oh, and you are a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    //console.log(str);
+    console.log(millenial);
+    //console.log(add(2, 3));
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Fanni';
+calcAge(1991);
