2e75846426a29357d38bde23f4aa9ceac2b31f21
Fanni Farkas <farkasfannikatalin@gmail.com>
2023-08-16 22:30:27 +0200

    finish section 9

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -61,6 +61,18 @@ const restaurant = {
 
 // WORKING WITH STRINGS ----------------------------------------------------------------------
 
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+
+  const output = `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll(
+    '_',
+    ' '
+  )} ${getCode(from)} ${getCode(to)} ${time.replace(':', 'h')}`.padStart(30);
+  console.log(output);
+}
+
 // CODING CHALLENGE 4 ---------------------------------------------------------------------
 
 document.body.append(document.createElement('textarea'));
