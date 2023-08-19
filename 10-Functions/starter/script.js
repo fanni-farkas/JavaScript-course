be73c74c820b864796772a3fe60f53eaac52a17e
Fanni Farkas <farkasfannikatalin@gmail.com>
2023-08-19 17:53:22 +0200

    practice call, apply and bind methods, finish coding challenge 1

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -59,7 +59,6 @@ const newPassport = function (person) {
 
 newPassport(fanni);
 checkIn(flight, fanni);
-*/
 
 const oneWord = function (str) {
   return str.replace(/ /g, '').toLowerCase();
@@ -90,3 +89,172 @@ const high5 = function () {
 document.body.addEventListener('click', high5);
 
 ['cat', 'dog', 'rabbit'].forEach(high5);
+
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey'); // this value is the returned function
+
+greeterHey('you');
+
+greet('Hello')('you');
+
+// CHALLENGE
+
+// const greet = greeting => {
+//   return name => console.log(`${greeting} ${name}`);
+// };
+
+const greetArr = greeting => name => console.log(`${greeting} ${name}`);
+
+greetArr('Hi')('you');
+
+const lufthansa = {
+  airline: 'Lufthansa',
+  iataCode: 'LM',
+  bookings: [],
+  //book: function() {}
+  book(flightNum, name) {
+    console.log(
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
+    );
+    this.bookings.push({ flight: `${this.iataCode}${flightNum}, name` });
+  },
+};
+
+lufthansa.book(239, 'Fanni Farkas');
+lufthansa.book(239, 'John Smith');
+console.log(lufthansa);
+
+const eurowings = {
+  airline: 'Eurowings',
+  iataCode: 'EW',
+  bookings: [],
+};
+
+const book = lufthansa.book;
+
+// Does not work
+//book(23, 'Sarah Williams')
+
+// CALL METHOD
+book.call(eurowings, 23, 'Sarah Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'Mary Cooper');
+console.log(lufthansa);
+
+const swiss = {
+  airline: 'Swiss Air Lines',
+  iataCode: 'LX',
+  bookings: [],
+};
+
+book.call(swiss, 583, 'Mary Cooper');
+console.log(swiss);
+
+// APPLY METHOD - same as call but it takes an array of the arguments
+const flightData = [583, 'George Cooper'];
+
+book.apply(swiss, flightData);
+console.log(swiss);
+
+book.call(swiss, ...flightData); // more useful, shorter
+
+// BIND METHOD
+// - does not immediately call the function, instead it returns a new function where the this keyword is bound (so it's set to whatever value we pass into bind)
+
+//book.call(eurowings, 23, 'Sarah Williams');
+
+const bookEW = book.bind(eurowings);
+const bookLM = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'Steven Williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Fanni Farkas');
+bookEW23('Martha Cooper');
+
+// OBJECTS WITH EVENT LISTENERS
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+
+  this.planes++;
+  console.log(this.planes);
+};
+//lufthansa.buyPlane();
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// PARTIAL APPLICATION
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+// addVAT = value => value + value * 0.23;
+
+console.log(addVAT(100));
+console.log(addVAT(23));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
+*/
+
+// CODING CHALLENGE 1
+
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  answers: new Array(4).fill(0),
+
+  registerNewAnswer() {
+    // Get answer
+    const answer = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+      )
+    );
+
+    console.log(answer);
+
+    // Register answer
+    typeof answer === 'number' &&
+      answer < this.answers.length &&
+      this.answers[answer]++;
+
+    this.displayResults();
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
+};
+
+// poll.registerNewAnswer();
+
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+poll.displayResults.call({ answers: [5, 2, 3] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] }, 'string');
+poll.displayResults.call({ answers: [1, 5, 3, 9, 6, 1] });
